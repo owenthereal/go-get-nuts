5f7b10660bbbcd6ad3191524bd1d7c62cc8f1cb2
Jingwen Owen Ou <jingweno@gmail.com>
2015-07-09 01:18:33 -0600

    Don't load background

--- js/load.js
+++ js/load.js
@@ -12,7 +12,7 @@ var loadState = {
     game.load.image('enemy', 'assets/bomb.png');
     game.load.image('coin', 'assets/nut.png');
     game.load.image('pixel', 'assets/pixel.png');
-    game.load.image('background', 'assets/background.png');
+    //game.load.image('background', 'assets/background.png');
     game.load.spritesheet('mute', 'assets/muteButton.png', 28, 22);
     game.load.image('jumpButton', 'assets/jumpButton.png');
     game.load.image('rightButton', 'assets/rightButton.png');
